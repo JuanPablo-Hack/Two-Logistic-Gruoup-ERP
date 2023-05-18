06fca37b04d32e4e092ace8958433ce4ceb94f53
JuanPabloHack <juanpablodejesusfigueroa@gmail.com>
2023-05-18 13:51:33 -0600

    Subiendo cambios a produccion

--- Admin/templates/modals/usuarios/agregar.php
+++ Admin/templates/modals/usuarios/agregar.php
@@ -6,15 +6,20 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <div class="alert alert-success" role="alert" style="display:none;" id="success">Usuario agregado con éxito!</div>
-                <div class="alert alert-danger" role="alert" style="display:none;" id="decline">Tuvimos un problema con la base de datos revisa tus datos, por favor!</div>
+                <div class="alert alert-success" role="alert" style="display:none;" id="success">Usuario agregado con
+                    éxito!</div>
+                <div class="alert alert-danger" role="alert" style="display:none;" id="decline">Tuvimos un problema con
+                    la base de datos revisa tus datos, por favor!</div>
                 <form id="AgregarUsuario">
                     <div class="row">
                         <div class="col mb-3">
                             <label class="form-label" for="basic-icon-default-fullname">Nombre Completo</label>
                             <div class="input-group input-group-merge">
-                                <span id="basic-icon-default-fullname2" class="input-group-text"><i class="bx bx-user"></i></span>
-                                <input type="text" class="form-control" id="basic-icon-default-fullname" aria-label="John Doe" aria-describedby="basic-icon-default-fullname2" name="nombre" required />
+                                <span id="basic-icon-default-fullname2" class="input-group-text"><i
+                                        class="bx bx-user"></i></span>
+                                <input type="text" class="form-control" id="basic-icon-default-fullname"
+                                    aria-label="John Doe" aria-describedby="basic-icon-default-fullname2" name="nombre"
+                                    required />
                             </div>
                         </div>
                     </div>
@@ -22,15 +27,20 @@
                         <div class="col mb-0">
                             <label class="form-label" for="basic-icon-default-company">Cargo</label>
                             <div class="input-group input-group-merge">
-                                <span id="basic-icon-default-company2" class="input-group-text"><i class="bx bx-buildings"></i></span>
-                                <input type="text" id="basic-icon-default-company" class="form-control" aria-label="ACME Inc." aria-describedby="basic-icon-default-company2" name="cargo" required />
+                                <span id="basic-icon-default-company2" class="input-group-text"><i
+                                        class="bx bx-buildings"></i></span>
+                                <input type="text" id="basic-icon-default-company" class="form-control"
+                                    aria-label="ACME Inc." aria-describedby="basic-icon-default-company2" name="cargo"
+                                    required />
                             </div>
                         </div>
                         <div class="col mb-0">
                             <label class="form-label" for="basic-icon-default-email">Email</label>
                             <div class="input-group input-group-merge">
                                 <span class="input-group-text"><i class="bx bx-envelope"></i></span>
-                                <input type="text" id="basic-icon-default-email" class="form-control" aria-label="john.doe" aria-describedby="basic-icon-default-email2" name="email" required />
+                                <input type="text" id="basic-icon-default-email" class="form-control"
+                                    aria-label="john.doe" aria-describedby="basic-icon-default-email2" name="email"
+                                    required />
                                 <span id="basic-icon-default-email2" class="input-group-text">@example.com</span>
                             </div>
                             <div class="form-text">Tienes que poner un correo electrónico válido.</div>
@@ -40,20 +50,28 @@
                         <div class="col mb-0">
                             <label class="form-label" for="basic-icon-default-phone">Teléfono</label>
                             <div class="input-group input-group-merge">
-                                <span id="basic-icon-default-phone2" class="input-group-text"><i class="bx bx-phone"></i></span>
-                                <input type="text" id="basic-icon-default-phone" class="form-control phone-mask" aria-label="658 799 8941" aria-describedby="basic-icon-default-phone2" name="tel" required />
+                                <span id="basic-icon-default-phone2" class="input-group-text"><i
+                                        class="bx bx-phone"></i></span>
+                                <input type="text" id="basic-icon-default-phone" class="form-control phone-mask"
+                                    aria-label="658 799 8941" aria-describedby="basic-icon-default-phone2" name="tel"
+                                    required />
                             </div>
                         </div>
                         <div class="col mb-0">
-                            <label class="form-label" for="basic-default-password12">Password</label>
-                            <div class="input-group">
-                                <input type="password" class="form-control" id="basic-default-password12" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="basic-default-password2" name="contra" required />
-                                <span id="basic-default-password2" class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                            <div class="form-password-toggle">
+                                <label class="form-label" for="basic-default-password12">Contraseña</label>
+                                <div class="input-group">
+                                    <input type="password" class="form-control" id="basic-default-password12"
+                                        aria-describedby="basic-default-password2">
+                                    <span id="basic-default-password2" class="input-group-text cursor-pointer"><i
+                                            class="bx bx-hide"></i></span>
+                                </div>
                             </div>
                         </div>
                         <div class="col mb-0">
                             <label for="exampleFormControlSelect1" class="form-label">Rol de usuario</label>
-                            <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example" name="rol" required>
+                            <select class="form-select" id="exampleFormControlSelect1"
+                                aria-label="Default select example" name="rol" required>
                                 <option selected>Selecciona un rol</option>
                                 <option value="1">Administrador</option>
                                 <option value="2">Comercial</option>
