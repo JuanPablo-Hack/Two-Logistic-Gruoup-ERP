3d604d3b5d151b9d2b5051e83e16bde943ac57a4
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-05-22 14:10:39 -0600

    Corriengiendo el jquery

--- Operativo/listar_servicios.php
+++ Operativo/listar_servicios.php
@@ -89,7 +89,6 @@
         </div>
         <div class="layout-overlay layout-menu-toggle"></div>
     </div>
-    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
     <script src="../assets/vendor/libs/popper/popper.js"></script>
     <script src="../assets/vendor/js/bootstrap.js"></script>
     <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
