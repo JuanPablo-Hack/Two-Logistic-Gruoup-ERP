e695bc37f88bcf7d1acca9fe4d2bd7a2ba13976c
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-05-24 10:19:02 -0600

    Cambiando el titulo de cambiar contraseña

--- Admin/templates/modals/usuarios/cambiar_contraseña.php
+++ Admin/templates/modals/usuarios/cambiar_contraseña.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel3">Agregar Usuario</h5>
+                <h5 class="modal-title" id="exampleModalLabel3">Cambiar Contraseña</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -24,7 +24,7 @@
                 <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
                     Cerrar
                 </button>
-                <button type="submit" class="btn btn-primary">Agregar Usuario</button>
+                <button type="submit" class="btn btn-primary">Cambiar Contraseña</button>
             </div>
             </form>
         </div>
