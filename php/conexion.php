42ad4458e79c967ccea3ed0b40564e0dd738fa17
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-07-05 11:19:37 -0600

    Conexiones exitosas

--- php/conexion.php
+++ php/conexion.php
@@ -1,9 +1,10 @@
 <?php
 
-$nombreServidor = "sql726.main-hosting.eu";
+$nombreServidor = "localhost";
 $nombreUsuario = "u288448544_twlerp";
 $passwordBaseDeDatos = "%A4ocJIw@nw4DBGH8a96";
 $nombreBaseDeDatos = "u288448544_twlerp";
+
 
 
 $conexion = new mysqli($nombreServidor, $nombreUsuario, $passwordBaseDeDatos, $nombreBaseDeDatos);
