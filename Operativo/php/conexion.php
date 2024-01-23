32e35e896d16851322d54b4ff8e6f9572142d5ec
Mona Lisa <sistemas@porter.com.mx>
2024-01-23 11:46:27 -0600

    subiendo cambios

--- Operativo/php/conexion.php
+++ Operativo/php/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$nombreServidor = "sql726.main-hosting.eu";
+$nombreServidor = "srv1103.hstgr.io";
 $nombreUsuario = "u288448544_twlerp";
 $passwordBaseDeDatos = "%A4ocJIw@nw4DBGH8a96";
 $nombreBaseDeDatos = "u288448544_twlerp";
